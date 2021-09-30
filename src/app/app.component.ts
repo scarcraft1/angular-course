663111787c8ffa365c661039f0e9e768220b0fd2
Óscar Villas Pazos <ovillas@hiberus.com>
2021-09-30 16:37:52 +0200

    Explico funcionamiento de directivas estructurales y ng-container, ng-template y ng-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   componente = 'angular-course';
+  changeComponent() {
+    const components = ['angular-course','angular-course-2','angular-course-3']
+    this.componente = components[Math.floor(Math.random() * components.length)];
+  }
 }
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,6 +1,7 @@
 import { ContentComponent } from './content/content.component';
+import { TestComponent } from './test/test.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 
-const COMPONENTS = [ ContentComponent, ToolbarComponent ];
+const COMPONENTS = [ ContentComponent, ToolbarComponent, TestComponent ];
 
-export { COMPONENTS, ContentComponent, ToolbarComponent }
+export { COMPONENTS, ContentComponent, ToolbarComponent, TestComponent }
