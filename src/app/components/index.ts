8bcd2fc05dee5f77aa7b974113fa8a1cd589ea87
Óscar Villas Pazos <ovillas@hiberus.com>
2021-10-05 18:07:08 +0200

    Añado componente reactivo

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,7 +2,21 @@ import { ContentComponent } from './content/content.component';
 import { TestComponent } from './test/test.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { ReactiveComponent } from './reactive/reactive.component';
 
-const COMPONENTS = [ ContentComponent, ToolbarComponent, TestComponent, SearchBarComponent ];
+const COMPONENTS = [
+  ContentComponent,
+  ToolbarComponent,
+  TestComponent,
+  SearchBarComponent,
+  ReactiveComponent
+];
 
-export { COMPONENTS, ContentComponent, ToolbarComponent, TestComponent, SearchBarComponent }
+export {
+  COMPONENTS,
+  ContentComponent,
+  ToolbarComponent,
+  TestComponent,
+  SearchBarComponent,
+  ReactiveComponent
+}
